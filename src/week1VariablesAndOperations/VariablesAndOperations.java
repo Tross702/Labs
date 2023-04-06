6e5c826456226badbef407f0806e532c1a9230fd
Tross702 <126952199+Tross702@users.noreply.github.com>
2023-04-06 00:10:53 -0700

    Initial commit

---
+++ src/week1VariablesAndOperations/VariablesAndOperations.java
@@ -0,0 +1,28 @@
+package week1VariablesAndOperations;
+
+public class VariablesAndOperations {
+
+  public static void main(String[] args) {
+    
+    int availablePlaneSeats = 6;
+    
+    double costOfGroceries = 23.64;
+    
+    char middleInitial =  'G';
+    
+    boolean isHotOutside = false;
+    
+    String customerFirstName = "Sally";
+    
+    String streetAddress = "1234 W Easy St";
+    
+    System.out.println("Available seats left on the plane: " + availablePlaneSeats);
+    System.out.println (costOfGroceries + " is the price for groceries.");
+    System.out.println("The peron's middle initial is " + middleInitial);
+    System.out.println("It is hot outisde - " + isHotOutside);
+    System.out.println("The customer's first name is " + customerFirstName);
+    System.out.println(streetAddress + " is where the person live ");
+
+  }
+
+}
